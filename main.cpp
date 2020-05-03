581197ceb34b14413df6b45247b7ca45d6337ed7
alllef <alekz29@gmail.com>
2020-05-04 02:06:39 +0300

    working insertion algorithm

--- main.cpp
+++ main.cpp
@@ -8,53 +8,62 @@ public:
     TreeNode *rightAddress = nullptr;
     TreeNode *parent = nullptr;
     int data = NULL;
+    string color = "";
 
-    TreeNode(TreeNode *leftAddress, TreeNode *rightAddress, TreeNode *parent, int data) {
+    TreeNode(TreeNode *leftAddress, TreeNode *rightAddress, TreeNode *parent, int data, string color) {
         this->leftAddress = leftAddress;
         this->rightAddress = rightAddress;
         this->data = data;
         this->parent = parent;
+        this->color = color;
     }
+
+
 };
 
 class BinarySearchTree {
     TreeNode *root = nullptr;
 
-private:
-    void addSec(TreeNode *rootNode, TreeNode *addNode) {
-        if (root == nullptr) root = addNode;
+public:
+    void add(int data) {
+        if (root == nullptr) root = new TreeNode(nullptr, nullptr, nullptr, data, "black");
         else {
-            while (rootNode != nullptr) {
-                if (addNode->data > root->data) {
-                    if (rootNode->rightAddress != nullptr) rootNode = rootNode->rightAddress;
-                    else {
-                        addNode->parent = root;
-                        root->rightAddress = addNode;
-                        break;
-                    }
-                } else if (addNode->data < root->data) {
-                    if (rootNode->leftAddress != nullptr)rootNode = rootNode->leftAddress;
-                    else {
-                        addNode->parent = rootNode;
-                        rootNode->leftAddress = addNode;
-                        break;
-                    }
-                }
+            while (root != nullptr) {
 
-                while (addNode->parent != nullptr) {
-                    addNode = addNode->parent;
+                if (data > root->data) {
+                    if (root->rightAddress != nullptr) root = root->rightAddress;
+                    else {
+                        root->rightAddress = new TreeNode(nullptr, nullptr, root, data, "red");
+
+                        break;
+                    }
+                } else if (data < root->data) {
+                    if (root->leftAddress != nullptr)root = root->leftAddress;
+
+                    else {
+                        root->leftAddress = new TreeNode(nullptr, nullptr, root, data, "red");
+
+                        break;
+                    }
                 }
-                rootNode = addNode;
+            }
+        }
+        while (root->parent != nullptr) {
+            if (root->parent->color == "red") {
+                if (uncle(root)->color == "red") {
+                    root->parent->parent->color = "red";
+                    root->parent->color = "black";
+                    uncle(root)->color="black";
+                }
+                else if(uncle(root)->color=="black")
             }
         }
     }
 
-public:
-
-    void add(int data) {
-
-        auto *node = new TreeNode(nullptr, nullptr, nullptr, data);
-        addSec(root, node);
+    TreeNode *uncle(TreeNode *node) {
+        if (node->parent->parent->leftAddress != node->parent)
+            return node->parent->parent->leftAddress;
+        else return node->parent->parent->rightAddress;
     }
 
     bool findData(int data) {
@@ -103,6 +112,8 @@ int main() {
     myTree.add(56);
     myTree.add(87);
     myTree.add(46);
+    myTree.add(99);
+    myTree.add(15);
     return 0;
 }
 
